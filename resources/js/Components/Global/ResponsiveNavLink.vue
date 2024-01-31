1fd3d93ce1e151be0c12b5de1e426a3d542f9731
nhannt-dev <nguyentrongnhan04072000@gmail.com>
2024-01-31 20:12:35 +0700

    custom partition vue

--- resources/js/Components/Global/ResponsiveNavLink.vue
+++ resources/js/Components/Global/ResponsiveNavLink.vue
@@ -1,3 +1,9 @@
+<template>
+    <Link :href="href" :class="classes">
+    <slot />
+    </Link>
+</template>
+
 <script setup>
 import { computed } from 'vue';
 import { Link } from '@inertiajs/vue3';
@@ -18,9 +24,3 @@ const classes = computed(() =>
         : 'block w-full ps-3 pe-4 py-2 border-l-4 border-transparent text-start text-base font-medium text-gray-600 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300 transition duration-150 ease-in-out'
 );
 </script>
-
-<template>
-    <Link :href="href" :class="classes">
-        <slot />
-    </Link>
-</template>
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,8 +1,3 @@
-<script setup>
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head } from '@inertiajs/vue3';
-</script>
-
 <template>
     <Head title="Dashboard" />
 
@@ -20,3 +15,8 @@ import { Head } from '@inertiajs/vue3';
         </div>
     </AuthenticatedLayout>
 </template>
+
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+</script>
